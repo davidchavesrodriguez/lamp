e3ed16841c578932998b05b7cc757fc13b9f51f2
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-02-20 11:56:43 +0100

    dos

---
+++ app/sites/default/settings.php
@@ -0,0 +1,15 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'db',
+  'username' => 'user',
+  'password' => 'user',
+  'prefix' => 'drupal',
+  'host' => 'basedatos',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'nrHd-jTWH7HS6vUpUbOPn2ljJ3BLXDtQoaoLxAHg-qyIg1xRjLm9Z4C8oDbmCTyyDvCGpHURkQ';
+$settings['config_sync_directory'] = 'sites/default/files/config_qLc_aJLSkUJOc1tM6RxHgwgYVApOhYNTTqKHC5NhkGfAUUWf4uyNeZWOWdjREMmi4M7ujkPI9w/sync';
